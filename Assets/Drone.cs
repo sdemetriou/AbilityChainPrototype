b5a7eb61fe9689441977c92627b652f07cc5ed66
sdemetriou <inbox@nellos.net>
2026-09-13 19:01:02 +0300

    Added health bars to enemies
    Per playtester 1's feedback, I have implemented a simple health bar for
    enemies such that their health values are now more legible.

--- Assets/Drone.cs
+++ Assets/Drone.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class Drone : MonoBehaviour
 {
     [SerializeField] private int health = 20;
+    private int maxHP;
+    [SerializeField] private GameObject healthBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    void Start()
+    {
+        maxHP = health;
+    }
     public void absorbDamage(int dmgAmount)
     {
         health -= dmgAmount;
         if (health <= 0)
         {
+            Destroy(healthBar);
             Destroy(gameObject);
         }
+        healthBar.transform.localScale = new Vector3(health / (float)maxHP, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
     }
 }
